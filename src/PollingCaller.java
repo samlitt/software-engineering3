8a91ecffdea732e9118be9ff1ea077fc18647f11
Konrad <konrad.m.kappmeyer@gmail.com>
2024-11-04 15:15:31 +0100

    feat: implement PollingCaller.java to specs

--- src/PollingCaller.java
+++ src/PollingCaller.java
@@ -1,14 +1,23 @@
-import java.io.IOException;
-
 public class PollingCaller extends Thread {
     private boolean returned = false;
     public PollingCaller () { this.start(); }
+		Cesar cesar = new Cesar();
+
     public void run() {
         System.out.println(" entfernter Aufruf läuft u. wartet auf Taste ");
-        try {
-            System.in.read();
-        } catch (IOException e) { e.printStackTrace(); }
+				try {
+						int sleepTime = 4000 + (int)(Math.random() * (10000 - 4000));
+						Thread.sleep(sleepTime);
+				}
+				catch(Exception e) {
+						System.out.println(e);
+				}
+
         returned = true;
     }
     public boolean returned() { return returned; }
+
+		public String getString() {
+				return cesar.getString();
+		}
 }
