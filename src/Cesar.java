949a09b384d5d71489c2e8968a0f1f7f62910707
Konrad <konrad.m.kappmeyer@gmail.com>
2024-11-04 14:27:04 +0100

    mod: cesar is now just object

--- src/Cesar.java
+++ src/Cesar.java
@@ -1,6 +1,6 @@
 import java.util.concurrent.Callable;
 
-public class Cesar implements Callable<String> {
+public class Cesar {
 
     public static String encrypt(String plaintext, int shift) {
         StringBuilder ciphertext = new StringBuilder();
@@ -22,7 +22,7 @@ public class Cesar implements Callable<String> {
         return encrypt(ciphertext, 26 - shift);
     }
 
-    public String call() {
+    public String getString() {
         String string = "Lorem ipsum odor amet, consectetuer adipiscing elit. Conubia etiam tempor scelerisque rhoncus faucibus. Sagittis curabitur duis mauris proin proin hendrerit. Ornare quisque aliquet nisl consequat torquent ad mauris primis est. Ex volutpat placerat congue lacinia donec massa. Eu vivamus montes platea et blandit venenatis maximus vehicula tempor. Penatibus class blandit elementum feugiat porttitor dolor. Condimentum cubilia semper suscipit velit luctus ornare rutrum. Neque libero turpis iaculis maximus est quam sagittis ullamcorper. Mauris semper ut at elit justo dapibus diam ultricies turpis.\n" +
                 "\n" +
                 "Fusce himenaeos inceptos ullamcorper elementum vel porttitor quisque netus. Egestas duis vehicula consequat ac eros imperdiet. Hac eget in volutpat, facilisis luctus feugiat. Lobortis maecenas vivamus cubilia senectus placerat gravida dui. Vitae taciti nascetur fusce nisi phasellus convallis tortor tellus felis. Mauris maecenas vivamus consectetur; phasellus est vivamus per conubia! Tempus aenean ante magna aliquam ridiculus magna dapibus tempor. Venenatis consequat nec blandit mollis, accumsan fringilla urna habitant.\n" +
@@ -34,13 +34,6 @@ public class Cesar implements Callable<String> {
                 "Pulvinar fermentum fames praesent egestas consectetur pellentesque neque ligula. Interdum in lacinia torquent leo imperdiet. Senectus suspendisse magna dolor primis nullam eleifend suscipit. Imperdiet turpis mauris vehicula proin nisl fermentum inceptos netus. Lorem habitasse potenti torquent tellus proin mollis. In id nisl fringilla ante est libero elit.";
         String encryptedString = encrypt(string, 5);
 
-				try {
-						int sleepTime = 4000 + (int)(Math.random() * (10000 - 4000));
-						Thread.sleep(sleepTime);
-				}
-				catch(Exception e) {
-						System.out.println(e);
-				}
 				return encryptedString;
 				// System.out.println(encryptedString);
     }
